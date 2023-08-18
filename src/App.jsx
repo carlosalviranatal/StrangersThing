05999bb76a0efeea22ac685126f74fac4d3899d2
Charles Graham <charlesgraham@MacBook-Pro.attlocal.net>
2023-08-17 17:55:43 -0700

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-
+/*eslint-disable*/
 import './App.css'
+import React ( useState ) from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import posts from "./Posts";
+import Profile from "./Profile";
+import Login from "./Login";
+import Register from "./Register";
 
-function App() {
+
+export default function App() {
+  const [Posts, setPosts] = useState([])
+  const [profile, setProfile] = useState([])
+  const [isLoggedIn, setIsLoggedIn] = useState([])
+  const [register, setRegister] = useState([])
 
   return (
     <>
+        <Routes>
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/Profile" element={<Profile />} />
 
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
     </>
   )
 }
 
-export default App
+

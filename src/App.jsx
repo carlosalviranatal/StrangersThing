358a53bf084028b6d95584923345ca9ad47dbb46
CountSamar <trentjohnbrown@gmail.com>
2023-08-19 17:16:19 -0500

    added content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-
+import Login from './component/Login'
+import Authenticate from './component/Authenticate'
+import SignUp from './component/SignUp'
 import './App.css'
 
 function App() {
 
   return (
     <>
-
+  <Login />
+  <SignUp />
+  <Authenticate />
     </>
   )
 }

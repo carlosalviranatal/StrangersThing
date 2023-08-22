099e2fbbf2b25960fb4557ec1a571eb66fbbfb77
Ilda Koleci <ildahallaci95@gmail.com>
2023-08-22 17:48:57 -0500

    Added create a new post

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
 import NavBar from './components/NavBar';
-// import Posts from "./posts"
+ import Posts from "./posts"
 import Profile from './components/Profile';
-// import Login from './component/Login'
-// import Authenticate from './component/Authenticate'
-// import SignUp from './component/SignUp'
+import Login from './component/Login'
+ import Authenticate from './component/Authenticate'
+import SignUp from './component/SignUp'
 import { Routes, Route } from 'react-router-dom'
 
 export default function App() {
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect, useNavigate } from "react"
 const cohortName = "2305-FTB-ET-WEB-PT"
 const baseURL = `https://strangers-things.herokuapp.com/api/${cohortName}`
-// import { Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 import { fetchPosts } from "baseURL"
 const [searchParam, setSearchParam] = useState("")
 const navigate = useNavigate()
@@ -48,6 +48,99 @@ export default function Posts () {
       </div>
       
     );
+ 
+    export default function CreatePost () {
+      const [title, setTitle] = useState("");
+       const [description, setDescription] = useState("");
+       const [price, setPrice] = useState("bench");
+       const [location, setLocation] = useState("");
+      
+       async function handleSubmit(e) {
+          e.preventDefault()
+          const newPost = {
+            title,
+            description,
+            price,
+            location,
+          
+          }
+          const makePost = async () => {
+  
+            try {
+              const response = await fetch(`${BASE_URL}/posts`, {
+                method: "POST",
+                headers: {
+                  'Content-Type': 'application/json',
+                  
+                },
+                body: JSON.stringify({
+                  post: {
+                    title: "My favorite stuffed animal",
+                    description: "This is a pooh doll from 1973. It has been carefully taken care of since I first got it.",
+                    price: "$480.00",
+                    location: "Chicago",
+                   
+                  }
+                })
+              });
+              const result = await response.json();
+              console.log(result);
+              return result
+            } catch (err) {
+              console.error(err);
+            }
+          }
+          
+         Result
+          await createNewPost(newPost)
+          setTitle('')
+          setDescription('')
+          setPrice('')
+          setLocation('field')
+         
+          fetchAllPosts()
+        }
+        return (
+          <form onSubmit={handleSubmit}>
+            <h1>Add New Post </h1>
+            <label htmlFor="name">Title</label>
+            <input
+              type="text"
+              id="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <label htmlFor="description">setDescription</label>
+            <input
+              type="text"
+              id="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <label htmlFor="price">Price</label>
+            <input
+              type="text"
+              id="price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+            <label htmlFor="location">Location</label>
+            <select
+              id="location"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            > </select>
+             
+            
+            <button className="create">Create</button>
+          </form>
+        )
+      }
+ 
+ 
   };
+
+
+
   
  

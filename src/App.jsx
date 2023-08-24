2908d406c1d06dce4f752b087f5db126a03e24ee
CountSamar <trentjohnbrown@gmail.com>
2023-08-24 16:45:29 -0500

    Added Route paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,34 @@
 import './App.css'
 import NavBar from './components/NavBar';
-import Posts from "./posts"
+// import Posts from "./posts"
 import Profile from './components/Profile';
-// import Login from './component/Login'
+import Login from './component/Login'
 //  import Authenticate from './component/Authenticate'
-// import SignUp from './component/SignUp'
-import { Routes, Route } from 'react-router-dom'
+import SignUp from './component/SignUp'
+import { Routes, Route, Link } from 'react-router-dom'
 
 export default function App() {
 // console.log(Posts)
 
   return (
     <>
-{/* //   <Login />
-//   <SignUp />
-//   <Authenticate /> */}
-    <NavBar/>
+    <div className='navLog'>
+      <Link to='/login'>Login</Link>
+      <Link to='/signup'>Register Account</Link>
     <Routes>
-      {/* <Route path="/" element={<Posts/>} /> */}
+      <Route path='/' element={<Login />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<SignUp />} />
+    </Routes>
+    </div>
+  
+
+    {/* <NavBar/>
+    <Routes>
+      <Route path="/" element={<Posts/>} />
       <Route path="/posts" element={<Posts/>} />
       <Route path="/profile" element={<Profile/>}/>
-    </Routes>
+    </Routes> */}
 
     </>
   )

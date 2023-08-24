c9dfc94a339efe736bc503ec9af75b0279bd9736
Carlos Alvira Natal <carlosalviranatal@Carloss-MacBook-Air.local>
2023-08-24 07:36:30 -0500

    non-functional search bar added

--- src/components/PostMessages.jsx
+++ src/components/PostMessages.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { postMessage } from "../API";
 import { useParams } from "react-router-dom";
 
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,49 +1,53 @@
 /*eslint-disable*/
 // import { useState} from "react"
-import { useNavigate } from 'react-router-dom';
-import { Link } from "react-router-dom"
-import DeletePost from "./DeletePost"
-import { useEffect } from "react"
-import { useState } from "react"
+import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import DeletePost from './DeletePost'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { fetchPosts } from '../API'
 // const navigate = useNavigate()
 // import PostCard from "./PostCard";
 
 
 
-
-export default function Posts({posts, getPosts}) {
+export default function Posts({ posts, getPosts, setPosts }) {
   console.log(posts)
   const [updatedposts, setupdatedPosts] = useState(null)
-  // const displayPosts = searchParam
-  // ? posts.filter((post) =>
-  // post.id().includes(searchParam())
-  // )
-  // : posts;
 
-  useEffect(() => {
-    
-  }, [updatedposts])
-
-
-  
 
 
   return (
     <div>
-       <h1><Link to="/CreatePost">Add a post!</Link></h1>
-      
+      <form className="search-bar">
+        <label htmlFor="search-term">Search: </label>
+        <input name="search-term" type="text" placeholder="search" />
+      </form>
+      <h1>
+        <Link to="/CreatePost">Add a post!</Link>
+      </h1>
+
       {posts?.map((post) => {
         return (
           <div className="linkDiv">
-  <h1><Link to={`/post/${post._id}`}>{post.title}</Link></h1>
-  <p>{post.description}</p>
-  <p>{post.author.username}</p>
-  <p>{post.price}</p>
-  <p>{post.location}</p>
-  {post.isAuthor?<DeletePost postId={post._id} onDelete={DeletePost} setupdatedPosts={setupdatedPosts} getPosts={getPosts}/>: null}
-</div>
-        );
+            <h1>
+              <Link to={`/post/${post._id}`}>{post.title}</Link>
+            </h1>
+            <p>{post.description}</p>
+            <p>{post.author.username}</p>
+            <p>{post.price}</p>
+            <p>{post.location}</p>
+            {post.isAuthor ? (
+              <DeletePost
+                postId={post._id}
+                onDelete={DeletePost}
+                setupdatedPosts={setupdatedPosts}
+                getPosts={getPosts}
+              />
+            ) : null}
+          </div>
+        )
       })}
     </div>
-  );
+  )
 }

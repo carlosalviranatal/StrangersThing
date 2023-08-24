b65c19b2133eda2dc065ceda028aeeef1e1d2b5d
Carlos Alvira Natal <carlosalviranatal@Carloss-MacBook-Air.local>
2023-08-24 07:14:27 -0500

    added a error message for non-auth users attempting to post message

--- src/components/PostMessages.jsx
+++ src/components/PostMessages.jsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 const PostMessages = () => {
     const params = useParams();
   const [myMessage, setMyMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState('') 
 
   const handleChange = (e) => {
     setMyMessage(e.target.value)
@@ -14,6 +15,11 @@ const PostMessages = () => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     let token = localStorage.getItem("token");
+
+    if (!token) {
+      setErrorMessage('You must be logged in to send a message.') 
+    }
+
     if (token) {
         const returnedInfo = await postMessage(token, params.id, myMessage);
         // setMyMessage(returnedInfo);
@@ -25,6 +31,7 @@ const PostMessages = () => {
     <div>
       <h2>your message</h2>
       <form onSubmit={handleSubmit}>
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
         <textarea onChange={handleChange}></textarea>
         <button type="submit">Submit</button>
       </form>

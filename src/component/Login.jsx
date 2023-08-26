98cba43adbf33b925e6ef1b45426e4f9c472835c
CountSamar <trentjohnbrown@gmail.com>
2023-08-26 15:28:49 -0500

    Added toast message feature

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -2,13 +2,19 @@
 import { useState } from 'react';
 import { loginUser } from '../API';
 import { Link, useNavigate } from 'react-router-dom';
-
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 // eslint-disable-next-line react/prop-types
 const Login = ({username, setUsername, password, setPassword, setIsLoggedIn,}) => {
   const [errorMessage, setErrorMessage] = useState('')
   let navigate = useNavigate();
+
+  const showToastMessage = () => {
+    toast.success('Login Successful !', {
+      position: toast.POSITION.BOTTOM_CENTER
+    })
+  }
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -17,6 +23,7 @@ const Login = ({username, setUsername, password, setPassword, setIsLoggedIn,}) =
     const token = await loginUser(username, password);
     if (token) {
       setIsLoggedIn(true);
+      showToastMessage()
       localStorage.setItem('user', token);
       navigate('/Profile');
     } else {
